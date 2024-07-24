6b1a533780ff56e24fba0cf444ee4f7137a9d087
CarlosPerez2711 <carlosperez271101@gmail.com>
2024-07-23 23:26:44 -0500

    empresa semana11

---
+++ resources/views/personas.blade.php
@@ -0,0 +1,32 @@
+@extends('layout')
+
+@section('title','Personas')
+
+@section('content')
+<td><h2>Personas </h2>
+    <tr>
+        @auth
+        <td colspan="4">
+            <a href="{{route('personas.create')}}"> Crear Nueva Persona</a>
+        </td>
+        @endauth
+    </tr>
+    <tr>
+        @if ($personas)
+            @foreach ($personas as $persona)
+                <td>
+                    <a href="{{ route('personas.show', $persona) }}">
+                        {{ $persona->cPerNombre }} {{ $persona->cPerApellido }}</a>
+                </td>
+            @endforeach
+        @else
+                <td colspan="4">No hay personas</td>
+        @endif
+    </tr>
+    <tr>
+        <td colspan="4">
+            {{$personas->links('pagination::bootstrap-4')}}
+        </td>
+    </tr>
+    </td>
+@endsection
